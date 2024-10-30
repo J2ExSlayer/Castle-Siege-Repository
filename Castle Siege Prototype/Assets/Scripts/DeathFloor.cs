be4c594c9dff0e5fe6a4f4fec97a7db7528bf0b1
Chris2799 <148915217+Chris2799@users.noreply.github.com>
2024-10-30 13:57:44 -0700

    death floor script updated

--- Castle Siege Prototype/Assets/Scripts/DeathFloor.cs
+++ Castle Siege Prototype/Assets/Scripts/DeathFloor.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathFloor : MonoBehaviour
+public class Deathfloor : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawn_point;
+    // Start is called before the first frame update
+
+ 
 
     private void OnTriggerEnter(Collider other)
     {
         player.transform.position = respawn_point.transform.position;
     }
+
+
 }
